3dcf9939bff09a7464d8dcb9bceef21064ff8659
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-04-30 16:49:47 -0400

    dynamic board + readability

--- src/conway.py
+++ src/conway.py
@@ -1,37 +1,29 @@
 class Conway:
     def __init__(self):
-        self.grid = [[0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+        self.length = 101
+        self.grid = data = [[0]*self.length for _ in range(self.length)]
+        self.modify = [
+            [0, 0, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+            [0, 0, 0, 1, 0],
+            [0, 1, 1, 1, 0],
+            [0, 0, 0, 0, 0]]
+
+        for index, sub_array in enumerate(self.modify):
+            for sub_index, item in enumerate(sub_array):
+                self.grid[index][sub_index] = item
 
     def print_grid(self):
-        for i in self.grid:
-            for j in i:
-                print(j, end=" ")
+        for sub_array in self.grid:
+            for item in sub_array:
+                print(item, end=" ")
             print()
 
     def get_str(self):
         grid_string = ""
-        for i in self.grid:
-            for j in i:
-                grid_string += f"{j} "
+        for sub_array in self.grid:
+            for item in sub_array:
+                grid_string += f'{str(item)} '
             grid_string += "\n"
         return grid_string
 
@@ -44,9 +36,9 @@ class Conway:
         self.grid = cells
 
     def get_type(self, row, column):
-        if row in {0, len(self.grid) - 1} and column in {0, len(self.grid[0]) - 1}:
+        if row in {0, self.length - 1} and column in {0, self.length - 1}:
             return "corner"
-        elif row in {0, len(self.grid) - 1} or column in {0, len(self.grid[0]) - 1}:
+        elif row in {0, self.length - 1} or column in {0, self.length - 1}:
             return "edge"
         return "normal"
 
@@ -103,7 +95,9 @@ class Conway:
                 grid[row - 1][column + 1], grid[row][column - 1],
                 grid[row][column + 1], grid[row + 1][column - 1],
                 grid[row + 1][column], grid[row + 1][column + 1]]
+
         neighbours = sum(section)
         state = (state == 1 and neighbours in {2, 3}) or (state == 0 and neighbours == 3)
         return state+0
+
         
