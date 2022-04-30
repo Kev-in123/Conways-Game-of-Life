76807fa007168863a092c46ba7b261c4961492a4
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2022-04-30 16:42:56 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,33 +1,36 @@
 import pygame
 from conway import Conway
 
-c = Conway()
 
 def main():
+    c = Conway()
     pygame.init()
-    surface = pygame.display.set_mode((600, 600))
+    window_size = 600
+    delay_ms = 10
+    surface = pygame.display.set_mode((window_size, window_size))
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-        with open("level.txt", "w") as f:
-            f.write(c.get_str())
-        pygame.time.wait(100)
-        with open("level.txt", "r") as f:
-            lines = "".join(f.readlines()).split(" ")
-            x = 0
-            y = 0
-            for i in range(400):
-                if i % 20 == 0 and i != 0:
-                    y += 30
-                    x = 0
-                col = (0, 0, 0) if lines[i] == "1" else (255, 255, 255)
-                pygame.draw.rect(surface, col, pygame.Rect(x, y, 30, 30))
-                x += 30     
-            pygame.display.update()
+        pygame.time.wait(delay_ms)
+        lines = c.get_str().split(" ")
+        line_break = int(len(lines)**0.5)
+        size = window_size // line_break + 1
+        x = 0
+        y = 0
+        for i, j in enumerate(lines):
+            if i % line_break == 0 and i != 0:
+                y += size
+                x = 0
+            col = (0, 0, 0) if j == "1" else (255, 255, 255)
+            pygame.draw.rect(surface, col, pygame.Rect(x, y, size, size))
+            x += size
+        pygame.display.update()
+
         c.cycle()
-    return   
+    return
+
 
 if __name__ == '__main__':
     main()
